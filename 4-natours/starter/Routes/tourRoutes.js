147b93699cc9069ff4161bac4cab9411c1d49bec
teddyhabtamu <tewodroshabtamu29@gmail.com>
2024-11-13 11:59:04 +0300

    start learning the basics of MongoDB

--- 4-natours/starter/Routes/tourRoutes.js
+++ 4-natours/starter/Routes/tourRoutes.js
@@ -6,7 +6,7 @@ const {
   updateTour,
   deleteTour,
   checkID,
-  checkBody
+  checkBody,
 } = require('../controllers/tourController');
 
 // Create a checkBody middleware
@@ -17,7 +17,7 @@ const router = express.Router();
 
 router.param('id', checkID);
 
-router.route('/').get(getAllTours).post(checkBody,createTour);
+router.route('/').get(getAllTours).post(checkBody, createTour); 
 
 router.route(`/:id`).get(getTour).patch(updateTour).delete(deleteTour);
 
