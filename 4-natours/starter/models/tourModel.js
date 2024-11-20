873150dd40657be2349d5da53449a2d8d9ba259d
teddyhabtamu <tewodroshabtamu29@gmail.com>
2024-11-20 15:53:37 +0300

    learn about MongoDB document middleware

--- 4-natours/starter/models/tourModel.js
+++ 4-natours/starter/models/tourModel.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const slugify = require('slugify');
 
 const tourSchema = new mongoose.Schema(
   {
@@ -7,6 +8,7 @@ const tourSchema = new mongoose.Schema(
       required: [true, 'A tour must have a name'],
       unique: true,
     },
+    slug: String,
     duration: {
       type: Number,
       required: [true, 'A tour must have a duration'],
@@ -58,6 +60,22 @@ const tourSchema = new mongoose.Schema(
     toObject: { virtuals: true },
   },
 );
+
+// DOCUMENT MIDDLEWARE: runs before .save() and .create()
+tourSchema.pre('save', function (next) {
+  this.slug = slugify(this.name, { lower: true });
+  next();
+});
+
+tourSchema.pre('save', function (next) {
+  console.log('Will save document ...');
+  next(); 
+});
+
+tourSchema.post('save', function (doc, next) {
+  console.log(doc);
+  next();
+});
 
 // virtual properties can't used on queries
 tourSchema.virtual('durationWeeks').get(function () {
