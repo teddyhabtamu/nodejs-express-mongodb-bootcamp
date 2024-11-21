400259aa4e5e0a1cc9bbbed01e8708e989ef9f19
teddyhabtamu <tewodroshabtamu29@gmail.com>
2024-11-21 10:25:04 +0300

    learn about MongoDB Query Middleware

--- 4-natours/starter/models/tourModel.js
+++ 4-natours/starter/models/tourModel.js
@@ -54,6 +54,10 @@ const tourSchema = new mongoose.Schema(
       select: false,
     },
     startDates: [Date],
+    secretTour: {
+      type: Boolean,
+      default: false,
+    },
   },
   {
     toJSON: { virtuals: true },
@@ -67,13 +71,26 @@ tourSchema.pre('save', function (next) {
   next();
 });
 
-tourSchema.pre('save', function (next) {
-  console.log('Will save document ...');
-  next(); 
+// tourSchema.pre('save', function (next) {
+//   console.log('Will save document ...');
+//   next();
+// });
+
+// tourSchema.post('save', function (doc, next) {
+//   console.log(doc);
+//   next();
+// });
+
+// QUERY MIDDLEWARE:
+tourSchema.pre(/^find/, function (next) {
+  this.find({ secretTour: { $ne: true } });
+  this.start = Date.now();
+  next();
 });
 
-tourSchema.post('save', function (doc, next) {
-  console.log(doc);
+tourSchema.post(/^find/, function (docs, next) {
+  console.log('Query took:', Date.now() - this.start);
+  // console.log(docs);
   next();
 });
 
