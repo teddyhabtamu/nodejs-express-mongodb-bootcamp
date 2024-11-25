ab4126e68303d0132973367e47adada4366233fe
teddyhabtamu <tewodroshabtamu29@gmail.com>
2024-11-25 21:43:41 +0300

    learn about global error handling

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const morgan = require('morgan');
 const tourRouter = require('./Routes/tourRoutes');
 const userRouter = require('./Routes/userRoutes');
+const AppError = require('./utiles/appError');
+const globalErrorHandler = require('./controllers/errorContorller')
 
 const app = express();
 
@@ -31,26 +33,11 @@ app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
 app.all('*', (req, res, next) => {
-  // res.status(404).json({
-  //   status: 'Fail',
-  //   message: `Can't find ${req.originalUrl} on this server`,
-  // });
-  const err = new Error(`Can't find ${req.originalUrl} on this server`);
-  err.status = 'fail';
-  err.statusCode = 404;
-  next(err);
+  next(new AppError(`Can't find ${req.originalUrl} on this server`, 404));
 });
 
 //since it has 4 arguments express automatically identifies it is the error handling middleware
-app.use((err, req, res, next) => {
-  err.statusCode = err.statusCode || 500;
-  err.status = err.status || 'error';
-  console.log('this is error');
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message,
-  });
-});
+app.use(globalErrorHandler);
 
 // 4. Start the Server
 module.exports = app;
